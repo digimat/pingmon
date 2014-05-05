bfc8079d8dcf62b83802c73a6877894d95735172
digimat <fhess@splust.ch>
2014-05-05 18:47:33 +0200

    ras

--- pingmon-i162.py
+++ pingmon-i162.py
@@ -1,14 +1,11 @@
 import pingmon
 
-pmon=pingmon.PingMon()
+pmon=pingmon.PingMon('/var/log/pingmon.py')
 
 pmon.addHost('192.168.139.1')
-
 pmon.addHost('192.168.143.200')
 pmon.addHost('192.168.143.220')
 pmon.addHost('192.168.143.221')
 pmon.addHost('192.168.143.222')
 
 pmon.run()
-
-
